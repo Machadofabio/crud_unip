79674562ab59317fe8f99275d20e194eaf0f666a
Fabio Machado <fabio.a.machado@hotmail.com>
2021-11-25 18:44:23 -0300

    Juntando back com front

---
+++ Tela.aspx
@@ -0,0 +1,110 @@
+﻿    <%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Tela.aspx.cs" Inherits="View.Tela" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <link rel="stylesheet" type="text/css" href="styleSheet.css" />
+    <title>Crudnow | Cadastre-se agora</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div class="container">
+            <h1>Crudnow</h1>
+            <div class="divHeader">
+                <asp:Label runat="server" CssClass="label">Buscar pelo CPF</asp:Label>
+                <br />
+                <asp:TextBox ID="txtBuscarPeloCPF" CssClass="txtBox" runat="server" Width="180px" Height="24px" BorderWidth="1px"
+                    BorderColor="Gray"></asp:TextBox>
+                <asp:ImageButton ID="ImageButtonConsulte" runat="server" OnClick="btnConsulte_Click" Height="24px"
+                    ImageUrl="./images/search-icon.png" Width="24px" />
+                <asp:ImageButton ID="ImageButtonAltere" runat="server" OnClick="btnAltere_Click" Height="24px"
+                    ImageUrl="./images/edit-icon.png" Width="24px" />
+                <asp:ImageButton ID="ImageButtonExclua" runat="server" OnClick="btnExclua_Click" Height="24px"
+                    ImageUrl="./images/trash-icon.png" Width="24px" />
+            </div>
+            <asp:Table runat="server" Width="178px">
+                <asp:TableRow runat="server">
+                    <asp:TableCell>
+                        <asp:Label runat="server" CssClass="label">Nome</asp:Label>
+                        <br />
+                        <asp:TextBox ID="txtNome" CssClass="txtBox" runat="server" Width="180px" Height="24px" BorderWidth="1px"
+                            BorderColor="Gray"></asp:TextBox>
+                    </asp:TableCell>
+                    <asp:TableCell>
+                        <asp:Label runat="server" CssClass="label">CPF</asp:Label>
+                        <br />
+                        <asp:TextBox ID="txtCPF" CssClass="txtBox" runat="server" Width="180px" Height="24px" BorderWidth="1px"
+                            BorderColor="Gray"></asp:TextBox>
+                    </asp:TableCell>
+                </asp:TableRow>
+                <asp:TableRow runat="server">
+                    <asp:TableCell>
+                        <asp:Label runat="server" CssClass="label">Telefone</asp:Label>
+                        <br />
+                        <asp:TextBox ID="txtTelefone" CssClass="txtBox" runat="server" Width="180px" Height="24px" 
+                            BorderWidth="1px" BorderColor="Gray"></asp:TextBox>
+                    </asp:TableCell>
+                    <asp:TableCell>
+                        <asp:Label runat="server" CssClass="label" Text="Cel.">Tipo Tel.</asp:Label>
+                        <br />
+                        <asp:TextBox ID="txtTipoTelefone" CssClass="txtBox" runat="server" Width="180px" Height="24px"
+                            BorderWidth="1px" BorderColor="Gray" Text="Cel."></asp:TextBox>
+                    </asp:TableCell>
+                </asp:TableRow>
+                <asp:TableRow runat="server">
+                    <asp:TableCell>
+                        <asp:Label runat="server" CssClass="label">Logradouro</asp:Label>
+                        <br />
+                        <asp:TextBox ID="txtLogradouro" CssClass="txtBox" runat="server" Width="180px" Height="24px" 
+                            BorderWidth="1px" BorderColor="Gray"></asp:TextBox>
+                        <br />
+                    </asp:TableCell>
+                    <asp:TableCell>
+                        <asp:Label runat="server" CssClass="label">Nº</asp:Label>
+                        <br />
+                        <asp:TextBox ID="txtNumeroLogradouro" CssClass="txtBox" runat="server" Width="180px" Height="24px"
+                            BorderWidth="1px" BorderColor="Gray"></asp:TextBox>
+                        <br />
+                    </asp:TableCell>
+                </asp:TableRow>
+                <asp:TableRow runat="server">
+                    <asp:TableCell>
+                        <asp:Label runat="server" CssClass="label">Bairro</asp:Label>
+                        <br />
+                        <asp:TextBox ID="txtBairro" CssClass="txtBox" runat="server" Width="180px" Height="24px" 
+                            BorderWidth="1px" BorderColor="Gray"></asp:TextBox>
+                        <br />
+                    </asp:TableCell>
+                    <asp:TableCell>
+                        <asp:Label runat="server" CssClass="label">CEP</asp:Label>
+                        <br />
+                        <asp:TextBox ID="txtCEP" CssClass="txtBox" runat="server" Width="180px" Height="24px" BorderWidth="1px"
+                            BorderColor="Gray"></asp:TextBox>
+                        <br />
+                    </asp:TableCell>
+                </asp:TableRow>
+                <asp:TableRow runat="server">
+                    <asp:TableCell>
+                        <asp:Label runat="server" CssClass="label">Cidade</asp:Label>
+                        <br />
+                        <asp:TextBox ID="txtCidade" CssClass="txtBox" runat="server" Width="180px" Height="24px" BorderWidth="1px"
+                            BorderColor="Gray"></asp:TextBox>
+                        <br />
+                    </asp:TableCell>
+                    <asp:TableCell>
+                        <asp:Label runat="server" CssClass="label">UF</asp:Label>
+                        <br />
+                        <asp:TextBox ID="txtUF" CssClass="txtBox" runat="server" Width="180px" Height="24px" BorderWidth="1px"
+                            BorderColor="Gray"></asp:TextBox>
+                        <br />
+                    </asp:TableCell>
+                </asp:TableRow> 
+            </asp:Table>
+            <asp:Button ID="btnInsira" runat="server" Text="Cadastrar" BackColor="#0066CC" Width="385px" 
+                OnClick="btnInsira_Click" Height="36px" ForeColor="White" BorderWidth="0px" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ Tela.aspx.cs
@@ -0,0 +1,212 @@
+﻿using Crudnow_MVC;
+using Crudnow_MVC.controle;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace View
+{
+    public partial class Tela : System.Web.UI.Page
+    { 
+        string connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Crudnow;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+
+        Gerenciador gerenciador;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            gerenciador = new Gerenciador(BancoDeDados.Simulador);
+        }
+
+        protected void btnInsira_Click(object sender, EventArgs e) // cadastrar
+        {
+            Pessoa p = new Pessoa();
+            p.Nome = txtNome.Text;
+            p.Cpf = txtCPF.Text;
+            p.Telefones = txtTelefone.Text + txtTipoTelefone.Text;
+            string endereco = txtLogradouro.Text + txtNumeroLogradouro.Text + txtBairro.Text + txtCEP.Text + txtCidade.Text + txtUF.Text;
+            p.Endereco = endereco;
+            gerenciador.dao.insira(p);
+            txtNome.Text = "";
+            txtCPF.Text = "";
+            txtTelefone.Text = "";
+            txtLogradouro.Text = "";
+            txtNumeroLogradouro.Text = "";
+            txtBairro.Text = "";
+            txtCEP.Text = "";
+            txtCidade.Text = "";
+            txtUF.Text = "";
+
+            string sql =
+           "INSERT INTO PESSOA (NOME, CPF, ENDERECO) "
+               + "VALUES ('"
+               + txtNome.Text + "', '"
+               + txtCPF.Text + "', '"
+              
+               + "');";
+
+            SqlConnection con = new SqlConnection(connectionString);
+            SqlCommand cmd = new SqlCommand(sql, con);
+            cmd.CommandType = CommandType.Text;
+            con.Open();
+            try
+            {
+                int i = cmd.ExecuteNonQuery();
+                if (i > 0)
+                    Console.WriteLine("Seu cadastro foi realizado com sucesso!");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Erro: " + ex.ToString());
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+
+        protected void btnConsulte_Click(object sender, ImageClickEventArgs e) // consultar
+        {
+            List<Pessoa> pessoas;
+            if (txtNome.Text.Equals(""))
+            {
+                pessoas = gerenciador.dao.consulte(txtCPF.Text);
+            }
+            else
+            {
+                pessoas = gerenciador.dao.consulte(txtCPF.Text);
+            }
+
+            string sql = "SELECT * FROM bdcadastro WHERE ID=" + txtBuscarPeloCPF.Text;
+
+            SqlConnection con = new SqlConnection(connectionString);
+            SqlCommand cmd = new SqlCommand(sql, con);
+            cmd.CommandType = CommandType.Text;
+            SqlDataReader reader;
+            con.Open();
+
+            try
+            {
+                reader = cmd.ExecuteReader();
+                if (reader.Read())
+                {
+                    txtNome.Focus();
+                    txtNome.Text = reader[1].ToString();
+                    txtCPF.Text = reader[2].ToString();
+                  //  txtDDD.Text = reader[3].ToString();
+                    txtTelefone.Text = reader[4].ToString();
+                    txtTipoTelefone.Text = reader[5].ToString();
+                    txtLogradouro.Text = reader[6].ToString();
+                    txtNumeroLogradouro.Text = reader[7].ToString();
+                    txtBairro.Text = reader[8].ToString();
+                    txtCEP.Text = reader[9].ToString();
+                    txtCidade.Text = reader[10].ToString();
+                    txtUF.Text = reader[11].ToString();
+                }
+                else
+                    Console.WriteLine("ID não encontrado, verifique o número do ID que você pesquisou");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Erro: " + ex.ToString());
+            }
+            finally
+            {
+                con.Close();
+            }
+            txtBuscarPeloCPF.Text = "";
+        }
+
+        protected void btnAltere_Click(object sender, ImageClickEventArgs e) // editar
+        {
+            Pessoa p = new Pessoa();
+            p.Nome = txtNome.Text;
+            p.Cpf = txtCPF.Text;
+            string telefones = txtTelefone.Text + txtTipoTelefone.Text;
+            p.Telefones = telefones;
+            string endereco = txtLogradouro.Text + txtNumeroLogradouro.Text + txtBairro.Text + txtCEP.Text + txtCidade.Text + txtUF.Text;
+            p.Endereco = endereco;
+            gerenciador.dao.altere(p);
+
+            string sql =
+          "UPDATE bdcadastro SET NOME='" + txtNome.Text +
+          "', CPF= '" + txtCPF.Text +
+          // "', DDD='" + txtDDD.Text +
+          "', TELEFONE='" + txtTelefone.Text +
+          "', TIPO_TELEFONE='" + txtTipoTelefone.Text +
+          "', ENDERECO= '" + txtLogradouro.Text +
+          "', NUMERO='" + txtNumeroLogradouro.Text +
+          "', BAIRRO='" + txtBairro.Text +
+          "', CEP='" + txtCEP.Text +
+          "', CIDADE= '" + txtCidade.Text +
+          "', UF='" + txtUF.Text +
+          "' WHERE ID=" + txtBuscarPeloCPF.Text;
+
+            SqlConnection con = new SqlConnection(connectionString);
+            SqlCommand cmd = new SqlCommand(sql, con); // recebe a instrução sql e a conexão com o BD
+            cmd.CommandType = CommandType.Text;
+            con.Open();
+            try
+            {
+                int i = cmd.ExecuteNonQuery();
+                if (i > 0)
+                    Console.WriteLine("Seus dados foram atualizados com sucesso!");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Erro: " + ex.ToString());
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+
+        protected void btnExclua_Click(object sender, ImageClickEventArgs e) // excluir
+        {
+            Pessoa p = new Pessoa();
+            p.Nome = txtNome.Text;
+            p.Cpf = txtCPF.Text;
+            string telefones = txtTelefone.Text + txtTipoTelefone.Text;
+            p.Telefones = telefones;
+            string endereco = txtLogradouro.Text + txtNumeroLogradouro.Text + txtBairro.Text + txtCEP.Text + txtCidade.Text + txtUF.Text;
+            p.Endereco = endereco;
+            gerenciador.dao.exclua(p);
+
+            string sql = "DELETE FROM bdcadastro WHERE ID=" + txtBuscarPeloCPF.Text;
+
+            SqlConnection con = new SqlConnection(connectionString);
+            SqlCommand cmd = new SqlCommand(sql, con);
+            cmd.CommandType = CommandType.Text;
+            con.Open();
+
+            try
+            {
+                int i = cmd.ExecuteNonQuery();
+                if (i > 0)
+                    Console.WriteLine("Registro excluído com sucesso!");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Erro: " + ex.ToString());
+            }
+            finally
+            {
+                con.Close();
+            }
+
+            txtNome.Text = "";
+            txtCPF.Text = "";
+            txtTelefone.Text = "";
+            txtLogradouro.Text = "";
+            txtNumeroLogradouro.Text = "";
+            txtBairro.Text = "";
+            txtCEP.Text = "";
+            txtCidade.Text = "";
+            txtUF.Text = "";
+        }
+    }
+}
